bac1befaaeb3c78ded9598b8d5f07cb11443f298
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-06-12 00:28:52 +0300

    Removed: unnecessary code

--- dev/dev.dev.js
+++ dev/dev.dev.js
@@ -3,7 +3,7 @@ require('../spec/spec.spec.js')
 require('./unit/functions/takePercent.dev.js')
 require('./unit/classes/character/Leveler.dev.js')
 require('./unit/abstract-fabric/abilities/abilityFabric.dev.js')
-require('./unit/abstract-fabric/activities/buffActitvityFabric.dev.js')
+require('./unit/abstract-fabric/activities/actitvityFabric.dev.js')
 
 require('./integration/cycle-aura.dev.js')
 require('./integration/cycle-buff.dev.js')
