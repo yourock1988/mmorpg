22a03d05934300ca6f0859003d0750845302fa40
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-06-30 19:52:46 +0300

    Dev: fixed Cargo test

--- dev/unit/classes/character/Cargo.dev.js
+++ dev/unit/classes/character/Cargo.dev.js
@@ -3,21 +3,21 @@ import Cargo from '../../../../src/classes/character/Cargo.js'
 
 function добавление_итемов_и_группировка_итемов() {
   const cargo = new Cargo()
-  cargo.addItem(itemFabric('Bone'))
-  cargo.addItem(itemFabric('Bone'))
-  cargo.addItem(itemFabric('Powder'))
-  cargo.addItem(itemFabric('Skel'))
-  cargo.addItem(itemFabric('Skel'))
+  cargo.addItem(itemFabric('piece', 'Bone'))
+  cargo.addItem(itemFabric('piece', 'Bone'))
+  cargo.addItem(itemFabric('piece', 'Powder'))
+  cargo.addItem(itemFabric('equipment', 'Axe Of Glory'))
+  cargo.addItem(itemFabric('equipment', 'Axe Of Glory'))
 
   console.assert(cargo.items.length === 5 && cargo.groupedItems.length === 4)
 }
 function удаление_несгруппированного_итема() {
   const cargo = new Cargo()
-  cargo.addItem(itemFabric('Bone'))
-  cargo.addItem(itemFabric('Bone'))
-  cargo.addItem(itemFabric('Powder'))
-  cargo.addItem(itemFabric('Skel'))
-  cargo.addItem(itemFabric('Skel'))
+  cargo.addItem(itemFabric('piece', 'Bone'))
+  cargo.addItem(itemFabric('piece', 'Bone'))
+  cargo.addItem(itemFabric('piece', 'Powder'))
+  cargo.addItem(itemFabric('equipment', 'Axe Of Glory'))
+  cargo.addItem(itemFabric('equipment', 'Axe Of Glory'))
   const idToRemove = cargo.groupedItems.at(-1)[0].id
 
   cargo.removeItemById(idToRemove)
@@ -26,11 +26,11 @@ function удаление_несгруппированного_итема() {
 }
 function удаление_сгруппированного_итема() {
   const cargo = new Cargo()
-  cargo.addItem(itemFabric('Bone'))
-  cargo.addItem(itemFabric('Bone'))
-  cargo.addItem(itemFabric('Powder'))
-  cargo.addItem(itemFabric('Skel'))
-  cargo.addItem(itemFabric('Skel'))
+  cargo.addItem(itemFabric('piece', 'Bone'))
+  cargo.addItem(itemFabric('piece', 'Bone'))
+  cargo.addItem(itemFabric('piece', 'Powder'))
+  cargo.addItem(itemFabric('equipment', 'Axe Of Glory'))
+  cargo.addItem(itemFabric('equipment', 'Axe Of Glory'))
   const idToRemove = cargo.groupedItems.at(0)[0].id
 
   cargo.removeItemById(idToRemove)
@@ -39,14 +39,13 @@ function удаление_сгруппированного_итема() {
 }
 function добавление_клонов_итемов_выбрасывает_ошибку() {
   const cargo = new Cargo()
-  let item = itemFabric('Powder')
+  let item = itemFabric('piece', 'Powder')
   cargo.addItem(item)
   cargo.addItem(item)
 }
 
 добавление_итемов_и_группировка_итемов()
 удаление_несгруппированного_итема()
-добавление_итемов_и_группировка_итемов()
 удаление_сгруппированного_итема()
 
 // добавление_клонов_итемов_выбрасывает_ошибку()
